8484f605b13b6efa08fd1c7a4fbe524dd5d9cf33
Andrew Douglass <aadouglass.dt@gmail.com>
2018-12-02 17:33:26 -0600

    Updated comment for the loops

--- d1p2.py
+++ d1p2.py
@@ -1,11 +1,10 @@
 #   load list of integers to sum in for loop
 freq_change = open("C:\\Users\\AndrewDesktop\\Advent\\freq.txt").read().split()
 
-#   loop through elements of freq_change array and sum together for answer to the advent calendar.
+#   loop through elements of freq_change array and sum together then store each iteration as frequency history to find the first repeat.
 i = 0
 n = 0
 freq_history = []
-
 while n < 1:
     for x in range(len(freq_change)): 
         i = i + int(freq_change[x])
