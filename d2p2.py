ca204a93c15ac0606cfe909201bb2fc7d21f3c17
Andrew Douglass <aadouglass.dt@gmail.com>
2018-12-03 07:59:35 -0600

    completed the stuff

--- d2p2.py
+++ d2p2.py
@@ -1,21 +1,38 @@
 box_codes = open("AoCd2").read().split()
 box_a = []
 box_b = []
-match_length = 0
+full_match_len = 0
+sim_match_len = 0
+found = 0
+
+# create the outer most loop to look at each box individually
 for x in range(len(box_codes)):
-  match_length = len(box_codes[x]) - 1
-  box_a = list(box_codes[x])
-  for y in range(len(box_codes)):
-    box_b = list(box_codes[y])
-    box_mismatch_count = 0
-    box_match = ''
-    for z in range(len(box_a)):
-      if box_a[z] == box_b[z]:
-        box_match = box_match + box_a[z]
-      elif ord(box_a[z]) - ord(box_b[z]) == 1 or ord(box_a[z]) - ord(box_b[z]) == -1:
-        box_mismatch_count = box_mismatch_count + 1
-        if box_mismatch_count > 1:
-          break
-      print(box_match)
-      if match_length == len(box_match):
-        print(box_match)
+# break the loops if we found our match
+    if found == 1:
+        break
+    else:
+	    full_match_len = len(box_codes[x])
+	    sim_match_len = len(box_codes[x]) - 1
+	    box_a = list(box_codes[x])
+# loop through the same list to compare each element to each other
+	    for y in range(len(box_codes)):
+# break the loops if we found our match
+	        if found == 1:
+	            break
+	        else:
+	    	    box_b = list(box_codes[y])
+	    	    box_mismatch_count = 0
+	    	    box_match = ''
+# compare each position in the box code to each other
+	    	    for z in range(len(box_a)):
+	    	        if x != y:
+	    	            if box_a[z] == box_b[z]:
+	    	    	    	box_match = box_match + box_a[z]
+	    	    	    else:
+	    	    	    	box_mismatch_count = box_mismatch_count + 1
+	    	    	    	if box_mismatch_count > 1:
+	    	    	    		break
+# we have found our match, look no further!
+	    	    	if len(box_match) == sim_match_len:
+	    	    	    print(box_match)
+	    	    	    found = 1
